2dbb1c4252b7749a675c670c8dddbde29ef036b8
JanashiRJ <96720@fhss.sjp.ac.lk>
2021-12-02 21:50:51 +0530

    Add all gift categories

---
+++ app/constants/Categories.js
@@ -0,0 +1,155 @@
+const category = [
+  {
+    cat: 'TEDDY',
+    cats: [
+      {
+        name: 'Toby Teddy Bears',
+        Price: 'RS 3500',
+        imageteddy: require('../assets/Images/Teddy/Toby.jpg'),
+      },
+      {
+        name: 'Big Cute and soft Plush Teddy Bear',
+        Price: 'RS 2500',
+        imageteddy: require('../assets/Images/Teddy/Plush.jpeg'),
+      },
+      {
+        name: 'Loving Teddy Cup',
+        Price: 'RS 2800',
+        imageteddy: require('../assets/Images/Teddy/lovingteddycup.jpg'),
+      },
+      {
+        name: 'Teddy Crystal',
+        Price: 'RS 3000',
+        imageteddy: require('../assets/Images/Teddy/crystal.jpg'),
+      },
+      {
+        name: 'Grand Fiesta',
+        Price: 'RS 2500',
+        imageteddy: require('../assets/Images/Teddy/Grand.jpg'),
+      },
+    ],
+  },
+  {
+    cat: 'FLOWER',
+    cats: [
+      {
+        name: 'A bouquet of natural flowers with a beautiful finishing for special lovers',
+        Price: 'RS 3000',
+        imageteddy: require('../assets/Images/Flowers/image1.jpg'),
+      },
+      {
+        name: ' Glass Dome Lamp Gift Box with red colour flower',
+        Price: 'RS 3000',
+        imageteddy: require('../assets/Images/Flowers/image2.jpg'),
+      },
+      {
+        name: 'Black-colour Rose Mini Bouquet with Artificial flowers',
+        Price: 'RS 3000',
+        imageteddy: require('../assets/Images/Flowers/image3.jpg'),
+      },
+      {
+        name: 'Pure white-roses-bouquet with fresh flowers',
+        Price: 'RS 3200',
+        imageteddy: require('../assets/Images/Flowers/image4.jpg'),
+      },
+      {
+        name: 'Beautiful flower arrangement made of artificial flowers and leaves',
+        Price: 'RS 3000',
+        imageteddy: require('../assets/Images/Flowers/image5.jpeg'),
+      },
+    ],
+  },
+  {
+    cat: 'CHOCOLATE',
+    cats: [
+      {
+        name: 'Chocolate ball gift box',
+        Price: 'RS 2500',
+        imageteddy: require('../assets/Images/Chocolate/image1.png'),
+      },
+      {
+        name: 'Classic Blue Chocolate Bow Gift Box',
+        Price: 'RS 2600',
+        imageteddy: require('../assets/Images/Chocolate/image2.png'),
+      },
+      {
+        name: 'Chocolate Gift Boxes',
+        Price: 'RS 1650',
+        imageteddy: require('../assets/Images/Chocolate/image3.png'),
+      },
+      {
+        name: 'Attractive And Quality Chocolate Gift Hamper',
+        Price: 'RS 2800',
+        imageteddy: require('../assets/Images/Chocolate/image4.png'),
+      },
+      {
+        name: 'Art Chocolate Packaging',
+        Price: 'RS 1850',
+        imageteddy: require('../assets/Images/Chocolate/image5.png'),
+      },
+    ],
+  },
+  
+  {
+    cat: 'WOODEN',
+    cats: [
+      {
+        name: 'Elegant table lamp with special wood finishing',
+        Price: 'RS 3500',
+        imageteddy: require('../assets/Images/Wooden/image1.jpg'),
+      },
+      {
+        name: 'Wooden Carved Chatri Ambari Elephant',
+        Price: 'RS 2800',
+        imageteddy: require('../assets/Images/Wooden/image6.jpg'),
+      },
+      {
+        name: 'Wooden ship Miniature Handicraft item for Gift',
+        Price: 'RS 2800',
+        imageteddy: require('../assets/Images/Wooden/image5.jpg'),
+      },
+      {
+        name: 'Souvenirs with exquisite wood arvings',
+        Price: 'RS 2800',
+        imageteddy: require('../assets/Images/Wooden/image3.jpg'),
+      },
+      {
+        name: 'Diary with a cover made of a special type of wood',
+        Price: 'RS 2800',
+        imageteddy: require('../assets/Images/Wooden/image4.jpg'),
+      },
+    ],
+  },
+  {
+    cat: 'WALL DECORATIONS',
+    cats: [
+      {
+        name: ' Macrame feather/leaves wall decoration ',
+        Price: 'RS 1500',
+        imageteddy: require('../assets/Images/Walldecorations/image1.jpg'),
+      },
+      {
+        name: 'Hand Painting Thunpath Rana. Fabric Velvet Wall Hanging Wall Art',
+        Price: 'RS 2800',
+        imageteddy: require('../assets/Images/Walldecorations/image2.jpg'),
+      },
+      {
+        name: 'Hand-painted wall decoration for House or hotel',
+        Price: 'RS 2800',
+        imageteddy: require('../assets/Images/Walldecorations/image3.png'),
+      },
+      {
+        name: 'Black and white clipart frame-wall-hanging ',
+        Price: 'RS 2800',
+        imageteddy: require('../assets/Images/Walldecorations/image4.png'),
+      },
+      {
+        name: 'Wall clock with beautiful finishing',
+        Price: 'RS 2800',
+        imageteddy: require('../assets/Images/Walldecorations/image5.png'),
+      },
+    ],
+  },
+];
+
+export default category;
