e5ed8c67d05c783915bded9674d3e141753996ff
JanashiRJ <96720@fhss.sjp.ac.lk>
2021-12-12 00:59:05 +0530

    Code Card in MoreDetails Screen

--- app/screens/MoreDetails.js
+++ app/screens/MoreDetails.js
@@ -1,14 +1,60 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  Dimensions,
+  FlatList,
+} from 'react-native';
 import Header from '../components/Moredetails/Header';
+import category from '../constants/OccationList';
+const {width} = Dimensions.get('screen');
+const cardWidth = width / 2 - 20;
 
-export default function Moredetails() {
+const Card = ({category}) => {
   return (
-    <SafeAreaView style={{backgroundColor: '#cdcfd1', flex: 1}}>
-      <View style={{backgroundColor: 'white', padding: 15}}>
-        <Header />
+    <View style={style.card}>
+      <View style={{alignItems: 'center'}}>
+        <Image
+          source={category.image}
+          style={{height: 100, width: 100, borderRadius: 40}}
+        />
       </View>
-    </SafeAreaView>
+      <View style={{marginHorizontal: 20}}>
+        <Text style={{fontSize: 15, fontWeight: 'bold', color: 'black'}}>
+          {category.title}
+        </Text>
+      </View>
+    </View>
   );
 };
 
+export default function Moredetails() {
+  return (
+    <SafeAreaView style={{backgroundColor: '#edf0ee', flex: 1}}>
+      <View style={{backgroundColor: 'white', padding: 15}}>
+        <Header />
+      </View>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        numColumns={2}
+        data={category}
+        renderItem={({item}) => <Card category={item} />}
+      />
+    </SafeAreaView>
+  );
+}
+const style = StyleSheet.create({
+  card: {
+    height: 220,
+    width: cardWidth,
+    marginHorizontal: 10,
+    marginBottom: 2,
+    marginTop: 20,
+    borderRadius: 15,
+    elevation: 10,
+    backgroundColor: 'white',
+  },
+});
