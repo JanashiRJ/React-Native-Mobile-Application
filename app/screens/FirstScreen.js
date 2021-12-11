d2f5d11be8ab459aaa1e5677afe6b662c901ba98
JanashiRJ <96720@fhss.sjp.ac.lk>
2021-12-11 15:05:49 +0530

    ShopCategories component and add details

--- app/screens/FirstScreen.js
+++ app/screens/FirstScreen.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 import FirstScreenHeadertab from '../components/FirstScreenHeadertab';
+import ShopCategories from '../components/ShopCategories';
 
 export default function FirstScreen() {
   return (
-    <SafeAreaView>
-      <FirstScreenHeadertab />
+    <SafeAreaView style={{backgroundColor: '#cdcfd1', flex: 1}}>
+      <View style={{backgroundColor: 'white', padding: 15}}>
+        <FirstScreenHeadertab />
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <ShopCategories />
+      </ScrollView>
     </SafeAreaView>
   );
 }
