a405aed16c65f59efa65bccc877e9eb433c84895
JanashiRJ <96720@fhss.sjp.ac.lk>
2021-12-11 16:01:13 +0530

    FirstScreen nevigate to HomeScreen

--- app/screens/FirstScreen.js
+++ app/screens/FirstScreen.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import {SafeAreaView, View} from 'react-native';
-import {ScrollView} from 'react-native-gesture-handler';
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
 import FirstScreenHeadertab from '../components/FirstScreenHeadertab';
 import ShopCategories from '../components/ShopCategories';
 
-export default function FirstScreen() {
+const SignInScreen = ({navigation}) => {
   return (
     <SafeAreaView style={{backgroundColor: '#cdcfd1', flex: 1}}>
       <View style={{backgroundColor: 'white', padding: 15}}>
         <FirstScreenHeadertab />
       </View>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <ShopCategories />
-      </ScrollView>
+      <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <ShopCategories />
+        </ScrollView>
+      </TouchableOpacity>
     </SafeAreaView>
   );
-}
+};
+export default SignInScreen;
