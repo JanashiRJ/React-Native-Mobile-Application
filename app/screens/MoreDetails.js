7b48577e4a6f7c50578dfacb4388371e26a9938f
JanashiRJ <96720@fhss.sjp.ac.lk>
2021-12-15 05:22:29 +0530

    Change card buttons styles in moredetails screen

--- app/screens/MoreDetails.js
+++ app/screens/MoreDetails.js
@@ -1,61 +1,65 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Image,
-  Text,
-  Dimensions,
-  FlatList,
-} from 'react-native';
+import {SafeAreaView, StyleSheet, Image, View, Text} from 'react-native';
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
 import Header from '../components/Moredetails/Header';
-import category from '../constants/CategoryList';
-const {width} = Dimensions.get('screen');
-const cardWidth = width / 2 - 20;
 
-const Card = ({category}) => {
-  return (
-    <View style={style.card}>
-      <View style={{alignItems: 'center'}}>
-        <Image
-          source={category.image}
-          style={{height: 130, width: 140, borderRadius: 20, marginTop: 12}}
-        />
-      </View>
-      <View style={{marginHorizontal: 20}}>
-        <Text
-          style={{
-            fontSize: 15,
-            fontWeight: 'bold',
-            color: 'black',
-            marginTop: 20,
-          }}>
-          {category.title}
-        </Text>
-      </View>
-    </View>
-  );
-};
 
-export default function Moredetails() {
+const Moredetails = ({navigation}) => {
   return (
     <SafeAreaView style={{backgroundColor: '#f5f7f5', flex: 1}}>
       <View style={{backgroundColor: 'white', padding: 15}}>
         <Header />
       </View>
-      <FlatList
-        showsVerticalScrollIndicator={false}
-        numColumns={2}
-        data={category}
-        renderItem={({item}) => <Card category={item} />}
-      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{flexDirection: 'row', alignSelf: 'center', marginTop: 5}}>
+          <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
+            <View style={style.imageBtn}>
+              <Image
+                style={style.imagestyle}
+                source={require('../assets/Images/List/image4.jpg')}
+              />
+              <Text style={style.text}>Birth Day Parties</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <View style={style.imageBtn}>
+              <Image
+                style={style.imagestyle}
+                source={require('../assets/Images/List/image2.jpg')}
+              />
+              <Text style={style.text}>Mothers day or Teachers day</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={{flexDirection: 'row', alignSelf: 'center', marginTop: 5}}>
+          <TouchableOpacity>
+            <View style={style.imageBtn}>
+              <Image
+                style={style.imagestyle}
+                source={require('../assets/Images/List/image3.jpg')}
+              />
+              <Text style={style.text}>Valentine days</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <View style={style.imageBtn}>
+              <Image
+                style={style.imagestyle}
+                source={require('../assets/Images/List/image1.jpg')}
+              />
+              <Text style={style.text}>Weddings or other special Parties</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
-}
+};
+
 const style = StyleSheet.create({
-  card: {
+  imageBtn: {
     height: 220,
-    width: cardWidth,
+    width: 150,
     marginHorizontal: 10,
     marginBottom: 2,
     marginTop: 20,
@@ -63,4 +67,24 @@ const style = StyleSheet.create({
     elevation: 10,
     backgroundColor: 'white',
   },
+  imagestyle: {
+    height: 130,
+    width: 140,
+    borderRadius: 20,
+    marginTop: 12,
+    alignContent: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 5,
+  },
+  text: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: 'black',
+    marginTop: 20,
+    marginHorizontal: 10,
+    textAlign: 'center',
+    justifyContent: 'center',
+  },
 });
+
+export default Moredetails;
