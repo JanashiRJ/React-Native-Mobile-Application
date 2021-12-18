4bfc83f7584cee3ae145cc29960a1b2ba5229735
JanashiRJ <96720@fhss.sjp.ac.lk>
2021-12-19 04:03:48 +0530

    Text changes add in firstscreenheadertab

--- app/components/Moredetails/FirstscreenHeader.js
+++ app/components/Moredetails/FirstscreenHeader.js
@@ -7,13 +7,15 @@ export default function Header() {
     <SafeAreaView>
       <Image
         style={{width: '100%', height: 150}}
-        source={require('../../assets/Images/GiftShop.jpg')}
+        source={require('../../assets/Images/GiftShop2.jpg')}
       />
+
       <View
         style={{marginTop: 10, alignItems: 'center', justifyContent: 'center'}}>
-        <Text style={{fontSize: 18, color: '#3b3737'}}>
-          Choose the Best Birth Day Gift Items  
+        <Text style={{fontSize: 13, color: '#3b3737'}}>
+          Choose the best Birth Day Gift Items for Birth Day Parties
         </Text>
+        <Text> ⭐⭐⭐</Text>
       </View>
     </SafeAreaView>
   );
