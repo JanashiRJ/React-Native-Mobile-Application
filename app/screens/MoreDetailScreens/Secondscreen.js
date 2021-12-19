23bfa81bdacb77ca78cd5461f0d0252189914c70
JanashiRJ <96720@fhss.sjp.ac.lk>
2021-12-19 06:24:37 +0530

    Items details add to cart

--- app/screens/MoreDetailScreens/Secondscreen.js
+++ app/screens/MoreDetailScreens/Secondscreen.js
@@ -1,6 +1,72 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Image,
+  FlatList,
+  StyleSheet,
+} from 'react-native';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
 import SecondscreenHeader from '../../components/Moredetails/SecondscreenHeader';
+
+const items = [
+  {
+    image_url: require('../../assets/Images/Screen2/image1.jpg'),
+    name: 'Jade Apple Teacher Award Plaque',
+    price: 'Rs 1500',
+  },
+  {
+    image_url: require('../../assets/Images/Screen2/image2.jpg'),
+    name: 'Teacher Retirement Award Rectangular Crystal Plaque ',
+    price: 'Rs 850',
+  },
+  {
+    image_url: require('../../assets/Images/Screen2/image3.jpg'),
+    name: ' Mothers Day gifts',
+    price: 'Rs 900',
+  },
+  {
+    image_url: require('../../assets/Images/Screen2/image4.jpg'),
+    name: '3D Night Light',
+    price: 'Rs 1600',
+  },
+];
+
+const Card = ({items}) => {
+  return (
+    <View style={style.menuItemStyle}>
+      <BouncyCheckbox
+        iconStyle={{borderColor: 'gray', borderRadius: 0}}
+        fillColor="#5f6885"
+      />
+      <View style={{width: 240, justifyContent: 'space-evenly'}}>
+        <Text style={style.titleStyle}>{items.name}</Text>
+        <Text
+          style={{
+            fontSize: 15,
+            fontWeight: 'bold',
+            color: 'black',
+            margin: 10,
+            marginTop: -20,
+          }}>
+          {items.price}{' '}
+        </Text>
+      </View>
+      <View>
+        <Image
+          source={items.image_url}
+          style={{
+            height: 100,
+            width: 100,
+            borderRadius: 18,
+            marginLeft: -35,
+            marginTop: 20,
+          }}></Image>
+      </View>
+    </View>
+  );
+};
 
 const Secondscreen = () => {
   return (
@@ -8,8 +74,44 @@ const Secondscreen = () => {
       <View style={{backgroundColor: 'white', padding: 15}}>
         <SecondscreenHeader />
       </View>
+      <FlatList data={items} renderItem={({item}) => <Card items={item} />} />
     </SafeAreaView>
   );
 };
 
 export default Secondscreen;
+
+const style = StyleSheet.create({
+  cardDetailsContainer: {
+    height: 50,
+    backgroundColor: 'red',
+    flex: 1,
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    padding: 20,
+    justifyContent: 'center',
+  },
+  cardImageContainer: {
+    height: 100,
+    width: 130,
+    backgroundColor: '#ffff',
+    borderRadius: 20,
+    marginRight: -50,
+  },
+  titleStyle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'black',
+    margin: 10,
+  },
+  menuItemStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: 10,
+    marginTop: 20,
+    backgroundColor: 'white',
+    height: 120,
+  },
+});
+
+
