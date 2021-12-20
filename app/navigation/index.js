0b2a41309579eaac51372f6ef1a9aabaa0a325f4
JanashiRJ <96720@fhss.sjp.ac.lk>
2021-12-20 09:22:44 +0530

    ReduxProvider import

--- app/navigation/index.js
+++ app/navigation/index.js
@@ -13,40 +13,62 @@ import Firstscreen from '../screens/MoreDetailScreens/Firstscreen';
 import Secondscreen from '../screens/MoreDetailScreens/Secondscreen';
 import Thirdscreen from '../screens/MoreDetailScreens/Thirdscreen';
 import Forthscreen from '../screens/MoreDetailScreens/Forthscreen';
+import {Provider as ReduxProvider} from 'react-redux';
+import configureStore from '../reducers/Store';
+
+const store = configureStore();
+
 const PrimaryNavigator = createStackNavigator();
 
 class App extends Component {
   render() {
     return (
-      <NavigationContainer>
-        <PrimaryNavigator.Navigator
-          headerMode={{}}
-          initialRouteName="SignInScreen">
-          <PrimaryNavigator.Screen
-            name="OnboardScreen"
-            component={OnboardScreen}
-          />
-          <PrimaryNavigator.Screen
-            name="SignInScreen"
-            component={SignInScreen}
-          />
-          <PrimaryNavigator.Screen
-            name="SignUpScreen"
-            component={SignUpScreen}
-          />
-          <PrimaryNavigator.Screen name="Firstscreen" component={Firstscreen} />
-          <PrimaryNavigator.Screen
-            name="Secondscreen"
-            component={Secondscreen}
-          />
-          <PrimaryNavigator.Screen name="FirstScreen" component={FirstScreen} />
-          <PrimaryNavigator.Screen name="HomeScreen" component={HomeScreen} />
-          <PrimaryNavigator.Screen name="MoreDetails" component={MoreDetails} />
-          <PrimaryNavigator.Screen name="HeaderTab" component={HeaderTab} />
-          <PrimaryNavigator.Screen name="Thirdscreen" component={Thirdscreen} />
-          <PrimaryNavigator.Screen name="Forthscreen" component={Forthscreen} />
-        </PrimaryNavigator.Navigator>
-      </NavigationContainer>
+      <ReduxProvider store={store}>
+        <NavigationContainer>
+          <PrimaryNavigator.Navigator
+            headerMode={{}}
+            initialRouteName="SignInScreen">
+            <PrimaryNavigator.Screen
+              name="OnboardScreen"
+              component={OnboardScreen}
+            />
+            <PrimaryNavigator.Screen
+              name="SignInScreen"
+              component={SignInScreen}
+            />
+            <PrimaryNavigator.Screen
+              name="SignUpScreen"
+              component={SignUpScreen}
+            />
+            <PrimaryNavigator.Screen
+              name="Firstscreen"
+              component={Firstscreen}
+            />
+            <PrimaryNavigator.Screen
+              name="Secondscreen"
+              component={Secondscreen}
+            />
+            <PrimaryNavigator.Screen
+              name="FirstScreen"
+              component={FirstScreen}
+            />
+            <PrimaryNavigator.Screen name="HomeScreen" component={HomeScreen} />
+            <PrimaryNavigator.Screen
+              name="MoreDetails"
+              component={MoreDetails}
+            />
+            <PrimaryNavigator.Screen name="HeaderTab" component={HeaderTab} />
+            <PrimaryNavigator.Screen
+              name="Thirdscreen"
+              component={Thirdscreen}
+            />
+            <PrimaryNavigator.Screen
+              name="Forthscreen"
+              component={Forthscreen}
+            />
+          </PrimaryNavigator.Navigator>
+        </NavigationContainer>
+      </ReduxProvider>
     );
   }
 }
